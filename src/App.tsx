157974a688fe221a20552f9c94644b136ad8cf05
José Lucas <joselucasa937@gmail.com>
2024-01-02 12:44:40 -0300

    feito a noticação de email e finalizado a v.1 do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import Header from './components/Header/page';
 import Home from './components/Home/page';
 import Projects from './components/Projects/page';
 import Skills from './components/Skills/page';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/ReactToastify.min.css';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <Home />
       <About />
--- src/components/Contact/page.tsx
+++ src/components/Contact/page.tsx
@@ -1,6 +1,77 @@
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { toast, Slide } from 'react-toastify';
 import './styled.css';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const notifySuccess = () => {
+    toast.success('Seu E-mail foi enviado!!', {
+      toastId: 'email',
+      position: 'top-right',
+      transition: Slide,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  };
+
+  const notifyError = () => {
+    toast.error('E-mail não enviado!!', {
+      toastId: 'error',
+      position: 'top-right',
+      transition: Slide,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
+  };
+
+  function sendEmail(e: any) {
+    e.preventDefault();
+
+    if (name === '' || email === '' || message === '') {
+      alert('Por favor, preencha as informações!');
+      return;
+    }
+
+    const templateParams = {
+      from_name: name,
+      email: email,
+      message: message,
+    };
+
+    emailjs
+      .send(
+        'service_frws0ii',
+        'template_02fsk65',
+        templateParams,
+        'YLCREEIkU1Or7li25'
+      )
+      .then(
+        (response) => {
+          notifySuccess();
+          setName('');
+          setEmail('');
+          setMessage('');
+          console.log(response.status, response.text);
+        },
+        (error) => {
+          notifyError();
+          console.log(error);
+        }
+      );
+  }
+
   return (
     <section className="contact" id="contact">
       <h2 className="heading">
@@ -11,10 +82,16 @@ function Contact() {
         ></span>
       </h2>
 
-      <form action="#">
+      <form onSubmit={sendEmail}>
         <div className="input-box">
           <div className="input-field">
-            <input type="text" placeholder="Full Name" required />
+            <input
+              type="text"
+              placeholder="Your Name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+            />
             <span className="focus"></span>
             <span
               className="animate scroll"
@@ -22,7 +99,13 @@ function Contact() {
             ></span>
           </div>
           <div className="input-field">
-            <input type="email" placeholder="Email Address" required />
+            <input
+              type="email"
+              placeholder="Email Address"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
             <span className="focus"></span>
             <span
               className="animate scroll"
@@ -38,6 +121,8 @@ function Contact() {
             cols={30}
             rows={10}
             placeholder="Your Message"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
             required
           ></textarea>
           <span className="focus"></span>
